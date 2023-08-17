6514670c23147ad83301cf0762384d280b8d8e7e
Azoula <arthur.azoula@etudiant.univ-reims.fr>
2023-08-17 16:31:17 +0200

    Merge remote-tracking branch 'origin/master'

--- MySpotifyFrontend/src/components/Navbar.js
+++ MySpotifyFrontend/src/components/Navbar.js
@@ -6,6 +6,7 @@ import LoginButton from './LoginButton';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ isLoggedIn }) => {
+  const [userData, setUserData] = useState(null)
   const [isProfileMenuOpen, setProfileMenuOpen] = useState(false);
   const profileMenuRef = useRef(null);
 
@@ -24,11 +25,22 @@ const Navbar = ({ isLoggedIn }) => {
   };
 
   useEffect(() => {
+
     document.addEventListener('click', handleDocumentClick);
 
     return () => {
       document.removeEventListener('click', handleDocumentClick);
     };
+  }, []);
+
+  useEffect(() => {
+
+    fetch('/spotify/me')
+    .then(response => response.json())
+    .then(data => {
+      setUserData(data)
+    })
+    
   }, []);
 
 
@@ -65,14 +77,20 @@ const Navbar = ({ isLoggedIn }) => {
             </Link>
           </li>
           <li>
-            {isLoggedIn ? (
-              <div className="relative" ref={profileMenuRef}>
-                <button onClick={toggleProfileMenu} className="focus:outline-none">
-                  <FontAwesomeIcon icon={faUser} className="mr-2 text-s transition duration-300" />
-                  Profil
+            {isLoggedIn && userData && userData.images ? (
+              <div className="relative">
+                <button onClick={toggleProfileMenu} className="focus:outline-none flex items-center group">
+                  <div className="w-10 h-10 rounded-full overflow-hidden border-2 border-gray-500 bg-green-500 group-hover:ring-2 group-hover:ring-offset-2 group-hover:ring-green-500">
+                    <img
+                      src={userData.images && userData.images.length > 0 ? userData.images[1].url : ''}
+                      alt="Profile"
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <span className="ml-2">{userData.display_name}</span>
                 </button>
                 {isProfileMenuOpen && (
-                  <ul className="absolute mt-2 bg-white text-gray-800 py-2 rounded">
+                  <ul className="absolute mt-2 right-0 bg-white text-gray-800 py-2 rounded z-10">
                     <li>
                       <a href="/settings" className="flex items-center py-2 px-4 text-sm hover:bg-gray-200 transition duration-300">
                         <FontAwesomeIcon icon={faCog} className="mr-2" />
